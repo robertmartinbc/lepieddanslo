e551cf2b9ef7bd390c7286192b02ce146e790cf4
Robert J Martin <robertmartinbc@shaw.ca>
2013-07-29 16:14:44 -0700

    First Commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+== Welcome to Le Pied dans L'O
+
+This website is for a rental property.
+
+You can view the images and availability and fill out a form to request a booking.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lepieddanslo::Application.config.secret_token = 'faa639eeaedfd095a155651432712543e892201a463562df7630b621a07d0a2710e77dd310f940ff996825a9ddf4e2658cebb5441f14e946388c11a3b44b6c98'
